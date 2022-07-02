d3449b2d9e55133d0601b7c5b4c48a8ed66aed01
MuhammedAky <muhammedaky24@gmail.com>
2022-07-02 17:10:12 +0300

    Acceleration added

--- script.js
+++ script.js
@@ -1,75 +1,130 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let x = 100;
-let y = 100;
+const BALLZ = [];
 
-//boolean variables - if true, ball moves in the direction
 let LEFT, UP, RIGHT, DOWN;
 
-function drawBall(x, y, r){
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, 2*Math.PI);
-    ctx.strokeStyle = "black";
-    ctx.stroke();
-    ctx.fillStyle = "red";
-    ctx.fill();
-    ctx.closePath();
+//velocity gets multiplied by (1-friction)
+let friction = 0.1;
+
+class Ball{
+    constructor(x, y, r){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.vel_x = 0;
+        this.vel_y = 0;
+        this.acc_x = 0;
+        this.acc_y = 0;
+        this.acceleration = 1;
+        this.player = false;
+        BALLZ.push(this);
+    }
+
+    drawBall(){
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.r, 0, 2*Math.PI);
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    //displaying the current acceleration and the velocity of the ball
+    display(){
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x + this.acc_x*100, this.y + this.acc_y*100);
+        ctx.strokeStyle = "green";
+        ctx.stroke();
+        ctx.closePath();
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(this.x + this.vel_x*10, this.y + this.vel_y*10);
+        ctx.strokeStyle = "blue";
+        ctx.stroke();
+        ctx.closePath();
+    }
 }
 
-//direction booleans become true if the arrow key is pressed
-canvas.addEventListener('keydown', function(e){
-    if(e.keyCode === 37){
-        LEFT = true;
-    }
-    if(e.keyCode === 38){
-        UP = true;
-    }
-    if(e.keyCode === 39){
-        RIGHT = true;
-    }
-    if(e.keyCode === 40){
-        DOWN = true;
-    }
-});
-
-//direction booleans become true if the arrow key is released
-canvas.addEventListener('keyup', function(e){
-    if(e.keyCode === 37){
-        LEFT = false;
-    }
-    if(e.keyCode === 38){
-        UP = false;
-    }
-    if(e.keyCode === 39){
-        RIGHT = false;
-    }
-    if(e.keyCode === 40){
-        DOWN = false;
-    }
-});
-
-//changes the x or y position depending on the boolean values
-function move(){
+function keyControl(b){
+    canvas.addEventListener('keydown', function(e){
+        if(e.keyCode === 37){
+            LEFT = true;
+        }
+        if(e.keyCode === 38){
+            UP = true;
+        }
+        if(e.keyCode === 39){
+            RIGHT = true;
+        }
+        if(e.keyCode === 40){
+            DOWN = true;
+        }
+    });
+    
+    canvas.addEventListener('keyup', function(e){
+        if(e.keyCode === 37){
+            LEFT = false;
+        }
+        if(e.keyCode === 38){
+            UP = false;
+        }
+        if(e.keyCode === 39){
+            RIGHT = false;
+        }
+        if(e.keyCode === 40){
+            DOWN = false;
+        }
+    });
+    
+    //if true, the accelertion component gets a certain value
     if(LEFT){
-        x--;
+        b.acc_x = -b.acceleration;
     }
     if(UP){
-        y--;
+        b.acc_y = -b.acceleration;
     }
     if(RIGHT){
-        x++;
+        b.acc_x = b.acceleration;
     }
     if(DOWN){
-        y++;
+        b.acc_y = b.acceleration;
     }
+    if(!UP && !DOWN){
+        b.acc_y = 0;
+    }
+    if(!RIGHT && !LEFT){
+        b.acc_x = 0;
+    }
+
+    //acceleration values added to the velocity components
+    b.vel_x += b.acc_x;
+    b.vel_y += b.acc_y;
+    //velocity gets multiplied by a number between 0 and 1
+    b.vel_x *= 1-friction;
+    b.vel_y *= 1-friction;
+    //velocity values added to the current x, y position
+    b.x += b.vel_x;
+    b.y += b.vel_y;
+
 }
 
-//main loop that runs around 60 times per second
 function mainLoop() {
-    ctx.clearRect(0, 0, 640, 480);
-    move();
-    drawBall(x, y, 20);
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    BALLZ.forEach((b) => {
+        b.drawBall();
+        if (b.player){
+            keyControl(b);
+        }
+        b.display();
+    });
     requestAnimationFrame(mainLoop);
 }
+
+let Ball1 = new Ball(200, 200, 100);
+Ball1.player = true;
+
 requestAnimationFrame(mainLoop);
